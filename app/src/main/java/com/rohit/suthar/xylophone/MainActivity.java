99f2bd50665375bbaf2f0d09fb471ceb7fd70395
Rohit Suthar <iamrohitsuthar@gmail.com>
2019-08-13 22:54:07 +0530

    migrate project to the androidx

--- app/src/main/java/com/rohit/suthar/xylophone/MainActivity.java
+++ app/src/main/java/com/rohit/suthar/xylophone/MainActivity.java
@@ -2,9 +2,8 @@ package com.rohit.suthar.xylophone;
 
 import android.media.AudioManager;
 import android.media.SoundPool;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
